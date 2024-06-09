a1c898cc0cfbe72b2c406eabbaaa94008c5606f0
Leonard Lesinski <leoqonline@gmail.com>
2024-06-09 11:41:43 +0200

    Added HSSP format

--- src/routes/formats/+page.svelte
+++ src/routes/formats/+page.svelte
@@ -10,7 +10,7 @@
 <h1>List of documented formats</h1>
 
 {#each data.pages as page}
-  <a href="/formats/{page.path}" id="{page.path}" class="block p-4 border border-black rounded-lg no-underline bg-white hover:bg-neutral-100">
+  <a href="/formats/{page.path}" id="{page.path}" class="block p-4 border border-black rounded-lg no-underline bg-white hover:bg-neutral-100 mt-4">
     <h3 class="text-2xl font-medium">{page.name}<span class="text-lg pl-2 text-neutral-400">by {page.developer}</span></h3>
     <p class="mt-2">
       {#each page.extensions as extension}
