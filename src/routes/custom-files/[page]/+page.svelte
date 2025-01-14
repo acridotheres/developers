97399af3a785ee73d0b4d25214177333acca5f95
Leonard Lesinski <84378319+Le0X8@users.noreply.github.com>
2025-01-14 14:46:40 +0100

    Create +page.svelte

---
+++ src/routes/custom-files/[page]/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let data;
+</script>
+
+<a href="https://github.com/acridotheres/developers/blob/main/src/pages/custom-files/{data.path}.md?plain=1" class="block w-full text-right mt-6" target="_blank">view this page on GitHub</a>
+
+  <h1 class="mt-0">{data.metadata.title} | Acridotheres for Developers</h1>
+
+    <p>Path: {data.metadata.path}</p>
+
+
+<svelte:component this={data.content} />
+
+<div class="flex w-full gap-2 border-t border-t-black pt-1">
+	<a href="/custom-files" class="grow no-underline block text-center">back to all pages</a>
+</div>
