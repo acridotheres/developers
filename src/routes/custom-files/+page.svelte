2652c0e6fea032d6eac84a3e95e4e4d482c278b4
Leonard Lesinski <84378319+Le0X8@users.noreply.github.com>
2025-01-14 15:00:57 +0100

    Create +page.svelte

---
+++ src/routes/custom-files/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let data: { pages: {
+    path: string;
+    title: string;
+    filePath: string;
+  }[] };
+</script>
+
+<svelte:head>
+  <title>List of custom file documentation pages | Acridotheres for Developers</title>
+</svelte:head>
+
+<h1>List of custom file documentation pages</h1>
+
+<ul>
+  {#each data.pages as page}
+    <li>
+    <a href="{`/custom-files/${page.path}`}">{page.title} ({page.filePath})</a>
+    </li>
+  {/each}
+</ul>
